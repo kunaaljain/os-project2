c329b9c713fd0db7b754541a5f2fd3b7ec0e74a3
windspook@gmail.com <windspook@gmail.com@395ab446-4781-11de-b449-6f096a66a0e6>
2009-05-27 18:24:45 +0000

    Discussed with Wenkai and get some todo lists.

--- trunk/pintos/src/userprog/syscall.c
+++ trunk/pintos/src/userprog/syscall.c
@@ -26,19 +26,59 @@ syscall_handler (struct intr_frame *f UNUSED)
 	  void* arg1;
 	  void* arg2;
 	  int retval;
-	  if (number == 0) {
-	  } else if (number == 1) {
-		  arg0 = ((int *)stack_p)+1;
-	  } else if (number == 2) {
-		  arg0 = ((int *)stack_p)+1;
-		  arg1 = ((int *)stack_p)+2;
-	  } else if (number == 3) {
-		  arg0 = ((int *)stack_p)+1;
-		  arg1 = ((int *)stack_p)+2;
-		  arg2 = ((int *)stack_p)+3;
+	  if (number == SYS_HALT) {
+		  halt();
+	  } else if (number == SYS_EXIT) {
+		  exit()
+	  } else if (number == SYS_EXEC) {
+
+	  } else if (number == SYS_WAIT) {
+
+	  } else if (number == SYS_CREATE) {
+
+	  } else if (number == SYS_REMOVE) {
+
+	  } else if (number == SYS_OPEN) {
+
+	  } else if (number == SYS_FILESIZE) {
+
+	  } else if (number == SYS_READ) {
+
+	  } else if (number == SYS_WRITE) {
+
+	  } else if (number == SYS_SEEK) {
+
+	  } else if (number == SYS_TELL) {
+
+	  } else if (number == SYS_CLOSE) {
+
 	  } else {
-		  printf("invalid system call number: %d!\n" + number);
+		  printf("Unsupported system call number: %d!\n" + number);
+		  thread_exit();
 	  }
+
+//	  else if (number == 1) {
+//		  arg0 = ((int *)stack_p)+1;
+//	  } else if (number == 2) {
+//		  arg0 = ((int *)stack_p)+1;
+//		  arg1 = ((int *)stack_p)+2;
+//	  } else if (number == 3) {
+//		  arg0 = ((int *)stack_p)+1;
+//		  arg1 = ((int *)stack_p)+2;
+//		  arg2 = ((int *)stack_p)+3;
+//	  } else {
+//		  printf("invalid system call number: %d!\n" + number);
+//	  }
   }
 //  thread_exit ();
+}
+
+//TODO add function to check the validation of pointers passed from user.
+
+bool check_user_pointer(void* p) {
+	return false;
+}
+
+static void halt(void) {
+
 }
