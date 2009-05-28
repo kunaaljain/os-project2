764eff3c9303093ea0ed64bba7dd347ff167b118
windspook <windspook@gmail.com>
2009-05-28 21:49:44 +0000

    1 Basiclly, Argument passing passed.To change the amount of parameters of process echo to proof. 2 syscall dispather (50%) 3 check user argument (50%) 4 modified echo.c

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -4,77 +4,114 @@
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 #include "lib/user/syscall.h"
+#include "threads/vaddr.h"
 
-static void syscall_handler (struct intr_frame *);
-bool check_user_pointer(void* );
-
-void
-syscall_init (void)
-{
-  intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+static void syscall_handler(struct intr_frame *);
+bool check_user_pointer(void*);
+int syscall_execute(int);
+void syscall_init(void) {
+	intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
-static void
-syscall_handler (struct intr_frame *f UNUSED)
-{
-  printf ("system call!\n");
-  void * stack_p = f->esp;
-  //get stack pointer, which is pointing onto top of stack
-  if (stack_p != NULL) {
-	  //get system call number
-	  int number = *((int *)stack_p);
-	  void* arg0;
-	  void* arg1;
-	  void* arg2;
-	  int retval;
-	  if (number == SYS_HALT) {
-	  } else if (number == SYS_EXIT) {
-//		  exit()
-	  } else if (number == SYS_EXEC) {
+/* dispatch the syscall request to certain syscall*/
+/* by Xiaoqi Cao*/
+static void syscall_handler(struct intr_frame *f UNUSED) {
+	printf("system call!\n");
+	void * stack_p = f->esp;
+	//get stack pointer, which is pointing onto top of stack
+	if (stack_p != NULL) {
+		//get system call number
 
-	  } else if (number == SYS_WAIT) {
+		hex_dump(stack_p - 32, stack_p + 32, 64, true);
 
-	  } else if (number == SYS_CREATE) {
+		int number = *((int *) stack_p);
 
-	  } else if (number == SYS_REMOVE) {
-
-	  } else if (number == SYS_OPEN) {
-
-	  } else if (number == SYS_FILESIZE) {
-
-	  } else if (number == SYS_READ) {
-
-	  } else if (number == SYS_WRITE) {
-
-	  } else if (number == SYS_SEEK) {
-
-	  } else if (number == SYS_TELL) {
-
-	  } else if (number == SYS_CLOSE) {
-
-	  } else {
-		  printf("Unsupported system call number: %d!\n" + number);
-		  thread_exit();
-	  }
-
-//	  else if (number == 1) {
-//		  arg0 = ((int *)stack_p)+1;
-//	  } else if (number == 2) {
-//		  arg0 = ((int *)stack_p)+1;
-//		  arg1 = ((int *)stack_p)+2;
-//	  } else if (number == 3) {
-//		  arg0 = ((int *)stack_p)+1;
-//		  arg1 = ((int *)stack_p)+2;
-//		  arg2 = ((int *)stack_p)+3;
-//	  } else {
-//		  printf("invalid system call number: %d!\n" + number);
-//	  }
-  }
-//  thread_exit ();
+		int retval = syscall_execute(number);
+	}
+	thread_exit();
 }
 
-//TODO add function to check the validation of pointers passed from user.
-
+/* check the validation of pointers passed from user */
+/* by Xiaoqi Cao*/
 bool check_user_pointer(void* p) {
-	return false;
+
+	bool err = false;
+
+	//check if it is NULL
+	if (p == NULL) {
+		err = true;
+	}
+	//check if it points to the kernel VM
+	if (p > PHYS_BASE) {
+		err = true;
+	}
+	//TODO check if it points to the unmapped VM
+	return err;
+}
+
+int syscall_execute(int number) {
+	printf("syscall number = %d\n", number);
+	void* arg0;
+	void* arg1;
+	void* arg2;
+	int retval;
+
+	if (check_user_pointer(arg0) && check_user_pointer(arg1)
+			&& check_user_pointer(arg2)) {
+		switch (number) {
+		case SYS_HALT:
+		case SYS_EXIT:
+		case SYS_EXEC:
+		case SYS_WAIT:
+		case SYS_CREATE:
+		case SYS_REMOVE:
+		case SYS_OPEN:
+		case SYS_FILESIZE:
+		case SYS_READ:
+		case SYS_WRITE:
+		case SYS_SEEK:
+		case SYS_TELL:
+		case SYS_CLOSE:
+		default:
+		}
+	}
+	//invalid pointer
+	else {
+		//TODO release resources
+
+		//thread exit;
+		thread_exit();
+	}
+
+//	if (number == SYS_HALT) {
+//
+//	} else if (number == SYS_EXIT) {
+//
+//	} else if (number == SYS_EXEC) {
+//
+//	} else if (number == SYS_WAIT) {
+//
+//	} else if (number == SYS_CREATE) {
+//
+//	} else if (number == SYS_REMOVE) {
+//
+//	} else if (number == SYS_OPEN) {
+//
+//	} else if (number == SYS_FILESIZE) {
+//
+//	} else if (number == SYS_READ) {
+//
+//	} else if (number == SYS_WRITE) {
+//
+//	} else if (number == SYS_SEEK) {
+//
+//	} else if (number == SYS_TELL) {
+//
+//	} else if (number == SYS_CLOSE) {
+//
+//	} else {
+//		printf("Unsupported system call number: %d!\n" + number);
+//		thread_exit();
+//	}
+
 }
