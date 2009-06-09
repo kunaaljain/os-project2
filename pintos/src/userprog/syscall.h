ce6e896a6326259180d9f25c1dbeead13d6f9c0b
windspook <windspook@gmail.com>
2009-06-09 20:32:24 +0000

    09-06-09

--- pintos/src/userprog/syscall.h
+++ pintos/src/userprog/syscall.h
@@ -3,14 +3,20 @@
 
 #include "lib/kernel/list.h"
 
-
+/* the item of fd_list head, fd_list is the fds held by a thread*/
 struct fds_stub {
 	struct list *l;
 	struct list_elem fds_elem;
 };
 
+/* the list of fd_list head, fd_list is the fds held by a thread*/
 struct list fds_ll;
 
+
+/* the item to record the file which will be deleted in future due to a process calls
+   the remove system call, but it will not be deleted now due to some process may refer it
+   , after all the process release its fd corresponding to the certain file, the file
+   this is for implementing the unix file removing mechanism.*/
 struct removing_file {
 	int fd;
 	struct thread *t;
@@ -18,9 +24,8 @@ struct removing_file {
 	struct list_elem r_elem;
 };
 
+/* the list of removing file*/
 struct list removing_list;
-
-//struct list exec_sema_list;
 
 void syscall_init (void);
 
